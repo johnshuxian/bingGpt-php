d3a9413e26bcad81ca260004f97de50b6a3335d3
JOHNS-MATE\zhang <1776243356@qq.com>
2023-04-11 15:40:44 +0800

    更新

--- app/Jobs/ChatGpt.php
+++ app/Jobs/ChatGpt.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Http\Services\A;
 use App\Http\Services\BaseService;
 use App\Http\Services\TelegramService;
 use Illuminate\Bus\Queueable;
@@ -38,6 +37,12 @@ class ChatGpt implements ShouldQueue
      */
     public function handle()
     {
+        // 一次只能执行一个任务
+        if (!Redis::connection()->client()->set('lock:' . static::class, 1, ['nx', 'ex' => 60])) {
+            // 如果有任务正在执行，延迟10秒再执行
+            dispatch(new static($this->params))->delay(now()->addSeconds(5));
+        }
+
         $message_id = $this->params['message']['message_id'] ?? '';
         $id         = $this->params['message']['chat']['id'] ?? 0;
 
@@ -46,6 +51,9 @@ class ChatGpt implements ShouldQueue
         if ($message_id && $id && Redis::connection()->client()->set('lock:' . $key, 1, ['nx', 'ex' => 300])) {
             TelegramService::getInstance()->telegram($this->params, 'chatGpt', '', '', $key);
         }
+
+        // 释放锁
+        Redis::connection()->client()->del('lock:' . static::class);
 
         BaseService::getInstance()->destroyAll();
     }
